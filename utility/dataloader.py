277de62a7072a82c7a307d6c2d8783b1e86b0328
toba949c <tobias.baier@mailbox.tu-dresden.de>
2024-03-05 16:50:43 +0100

    minor adds in filemanager.py (get_identifiers also return id names) // implemented dataloader.py (untested)

--- utility/dataloader.py
+++ utility/dataloader.py
@@ -2,11 +2,21 @@ import os
 import json
 import pandas as pd
 import numpy as np
+from pprint import pprint
+
+try:
+    from filemanager import FileManager
+except ModuleNotFoundError:
+    print("ATTENTION! Operating without FileManager! Only read functions will work!")
 
 
 class DataLoader:
 
-    def __init__(self):
+    cfile: FileManager
+
+    def __init__(self, cfile: FileManager):
+        self.cfile = cfile
+
         self.standards = {}
         self.presets = {}
         self.load_setup()
@@ -15,11 +25,90 @@ class DataLoader:
         try:
             with open("../setup/presets/dataloader_presets.json", "r") as of:
                 self.presets = json.load(of)
-
         except FileNotFoundError:
             raise FileNotFoundError("Could not load Dataloader-presets at"
                                     "'../setup/presets/dataloader_presets.json'!")
 
+        try:
+            with open("../setup/standards/dataloader_standards.json", "r") as of:
+                self.standards = json.load(of)
+        except FileNotFoundError:
+            raise FileNotFoundError("Could not load Dataloader-standards at"
+                                    "'../setup/standards/dataloader_standards.json'!")
+
+    def auto_read(self, path):
+
+        current_settings = {}
+
+        ending = path.split(".")[-1]
+        if ending in self.standards:
+            current_settings = self.standards[ending] | current_settings
+        else:
+            raise TypeError(f"The ending '{ending}' cannot be read!")
 
 
 
+        ids, id_names = self.cfile.get_identifiers(path, return_id_names=True)
+
+        for name, id in zip(id_names, ids):
+            if name in self.presets:
+                if id in self.presets[name]:
+                    for key in current_settings:
+                        if key in self.presets[name][id]:
+                            current_settings[key] = self.presets[name][id][key] | current_settings[key]
+
+        if "data_type" in current_settings["settings"]:
+            if current_settings["settings"]["data_type"] != ending:
+                print(f"The provided file type '.{ending}' does not match the expected file type "
+                      f"'.{current_settings['data_type']}'! Trying to read anyway.")
+            current_settings["settings"].pop("data_type")
+
+        pprint(current_settings)
+
+    def data_from_csv(self, filepath: str, **kwargs):
+
+        if not os.path.isfile(filepath):
+            raise FileNotFoundError(f"There is no file at {filepath}. Please remember to enter a full path!")
+
+        if "data-format" in kwargs:
+            read_params = kwargs["data-format"]
+        else:
+            read_params = {}
+
+        # read data
+        data = pd.read_csv(filepath, ** read_params)
+
+        # define some vars for clarity
+        swap_axes = False
+        if "swap_axes" in kwargs["settings"]:
+            swap_axes = kwargs["settings"]["swap_axes"]
+        bin_ret = False
+        if "bin_ret" in kwargs["settings"]:
+            bin_ret = kwargs["settings"]["bin_ret"]
+
+        if swap_axes:
+            if not bin_ret:
+                return data.to_numpy().swapaxes(0, 1)
+            else:
+                x, y = data.to_numpy().swapaxes(0, 1)
+                bin_width = x[1] - x[0]
+                x = x - 0.5 * bin_width
+                return y, np.append(x, x[-1] + bin_width)
+        else:
+            return data.to_numpy
+
+    def data_from_txt(self, filepath, **kwargs):
+
+        return self.data_from_csv(filepath, **kwargs)
+
+
+
+
+
+
+
+
+fi = FileManager("Z:\Studenten\Baier\sample-data")
+dl = DataLoader(fi)
+
+dl.auto_read("Z:\Studenten\Baier\sample-data\data\sev\sebis110\sev_sebis110_bng2s096_na22_530_15min_hist_good.txt")
--- utility/filemanager.py
+++ utility/filemanager.py
@@ -34,6 +34,15 @@ def test_identifier(text, identifier):
     # if previous if-statements did not return, return False, as no identifier has been found
     return False
 
+def remove_path_syntax(s):
+    if "\\" in s:
+        s = s.replace("\\", "/")
+
+    if "/" in s:
+        s = s.split("/")[-1]
+
+    return s
+
 
 class FileManager:
 
@@ -86,7 +95,7 @@ class FileManager:
 
             # dict is extracted from a json file -> keys should be allowed identifiers
             elif self.identifiers[identifier]["type"] == "from-catalogue":
-                with open(self.presets["identifiers"][identifier]["path"], "r") as of:
+                with open(self.path + self.presets["identifiers"][identifier]["path"], "r") as of:
                     self.identifiers[identifier]["content"] = json.load(of)
 
             # regex string is saved
@@ -154,26 +163,38 @@ class FileManager:
 
         return True
 
-    def get_identifiers(self, filename):
+    def get_identifiers(self, filename, return_id_names=False):
+
+        filename = remove_path_syntax(filename)
+
         check_ret = self.check_identifiers(filename)
         name_elements = filename.split("_")
+        id_names = name_elements.copy()
 
         # if not all identifier are given, cycle through the name and check if any are in there
         if not check_ret:
             print("WARNING: Filename does not contain all defined identifiers!")
             for i, element in enumerate(name_elements.copy()):
-                for identifier in self.identifiers.values():
+                for name, identifier in self.identifiers.items():
                     if test_identifier(element, identifier) is False:
                         name_elements[i] = False
+                        id_names[i] = False
                     else:
                         name_elements[i] = element
+                        id_names[i] = name
                         break
 
-            return [i for i in name_elements if i is not False]
+            if return_id_names:
+                return [i for i in name_elements if i is not False], [i for i in id_names if i is not False]
+            else:
+                return [i for i in name_elements if i is not False]
 
         # if all identifiers are at the specified locations, return them
         else:
-            return name_elements[0:len(self.identifiers)]
+            if return_id_names:
+                return name_elements[0:len(self.identifiers)], list(self.identifiers.keys())
+            else:
+                return name_elements[0:len(self.identifiers)]
 
     def get_data_path(self, filename, check_identifier=True, check_rest=True):
         """
@@ -294,9 +315,9 @@ class FileManager:
 
 
 
-a = FileManager("C:/Users/baier/OneDrive/Programmierprojekte/data_fiasco/")
-# print(a.get_identifiers("sev_bis105_bcg2s060_na22_hist_good.txt"))
-print(a.resort_data([1, 0]))
+# a = FileManager("Z:\Studenten\Baier\sample-data")
+# print(a.get_identifiers("sev_sebis110_bng2s096_na22_530_15min_hist_good.txt"))
+# print(a.resort_data([1, 0]))
 
 
 
